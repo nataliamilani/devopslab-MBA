8f82b5e29536f2bc13c52026d0bfca80b4577cac
Natalia Milani <nataliamilani@users.noreply.github.com>
2021-09-23 22:21:38 -0300

    Fix python version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Natalia Milani"
+    return "Natalia Milani - Aula DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
